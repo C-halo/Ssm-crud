954063bdba18f4aa3273d66c23107749bcb9bfff
Halo <halo@123.com>
2021-07-11 16:30:25 +0800

    push test

---
+++ src/main/java/com/halo/utils/main.java
@@ -0,0 +1,14 @@
+package com.halo.utils;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author halo
+ * @Description:
+ * @date 2021/7/11, 001116:29
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("push test");
+    }
+}
